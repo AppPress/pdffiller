cce7165cda436ce7e3cfefcc80d789c94913f687
Adam P. Bell <abell@cslico.com>
2016-01-12 15:52:56 -0500

    Refactored a number of items in this library. getFDF now outputs actual FDF strings, per the spec here: http://www.adobe.com/content/dam/Adobe/en/devnet/acrobat/pdfs/fdf_data_exchange.pdf. Added support to return a listing of the fields in a pdf. Cleaned up test cases and test data. Added mocha and moved shouldjs as a development dependency in package.son.  Removed attributes in package.json that I couldn't find in the package.json spec. Updated README.md and fixed a number of syntax and grammatical errors.

--- index.js
+++ index.js
@@ -6,138 +6,81 @@
 *   Description: This PDF filler module takes a data set and creates a filled out
 *                PDF file with the form fields populated.
 */
-(function(){
-    var child_process = require('child_process'),
-        exec = require('child_process').exec,
-        fdf = require('fdf'),
-        _ = require('lodash'),
-        fs = require('fs');
+"use strict";
+var child_process = require("child_process"),
+    exec = child_process.exec,
+    fdf = require("fdf"),
+    fs = require("fs");
 
-    var pdffiller = {
+var pdffiller = {
 
-        mapForm2PDF: function( formFields, convMap ){
-            var tmpFDFData = this.convFieldJson2FDF(formFields);
-            tmpFDFData = _.mapKeys(tmpFDFData, function(value, key){
-                try {
-                    convMap[key];
-                } catch(err){
+    getFdf: function(sourceFile, callback){
+        this.getFormFields(sourceFile, function(err, formFields){
+           if(err){
+               return callback(err);
+           }
+           var fields = {};
+           formFields.forEach(function(formField){
+               fields[formField.fieldName] = "";
+           });
+           return callback(null, fdf.generate(fields));
+        });
+    },
 
-                    return key;
-                } 
-                return convMap[key];
-            });
-
-            return tmpFDFData;
-        },
-
-        convFieldJson2FDF: function(fieldJson){
-            var _keys = _.pluck(fieldJson, 'title'),
-                _values = _.pluck(fieldJson, 'fieldValue');
-
-            _values = _.map(_values, function(val){
-                if(val === true){
-                    return 'Yes';
-                }else if(val === false) {
-                    return 'Off';
-                }
-                return val;
-            });
-
-            var jsonObj = _.zipObject(_keys, _values);
-
-            return jsonObj;
-        },
-
-        generateFieldJson: function( sourceFile, nameRegex, callback){
-            var regName = /FieldName: ([^\n]*)/,
-                regType = /FieldType: ([A-Za-z\t .]+)/,
-                regFlags = /FieldFlags: ([0-9\t .]+)/,
-                fieldArray = [],
-                currField = {};
-            
-            if(nameRegex !== null && (typeof nameRegex) == 'object' ) regName = nameRegex;
-
-            exec( "pdftk " + sourceFile + " dump_data_fields_utf8 " , function (error, stdout, stderr) {
-                if (error) {
-                    console.log('exec error: ' + error);
-                    return callback(error, null);
-                }
-                
-                fields = stdout.toString().split("---").slice(1);
-                fields.forEach(function(field){
-                    currField = {};
-                    
-                    currField['title'] = field.match(regName)[1].trim() || ''; 
-
-                    if(field.match(regType)){
-                        currField['fieldType'] = field.match(regType)[1].trim() || '';  
-                    }else {
-                        currField['fieldType'] = '';
+    getFormFields: function (sourceFile, callback) {
+        var pdftkCommand = "pdftk " + sourceFile + " dump_data_fields_utf8";
+        exec(pdftkCommand, function (err, stdout, stderr) {
+            if (err) {
+                console.log(err);
+                return callback(err);
+            }
+            var fieldSections = stdout.toString().split("---").slice(1);
+            var formFields = fieldSections.map(function (fieldSection) {
+                var fieldAttributes = fieldSection.split(/\n/);
+                var result = {};
+                fieldAttributes.forEach(function (fieldAttribute) {
+                    var packedAttribute = fieldAttribute.split(/:/);
+                    var key = packedAttribute[0];
+                    var value = packedAttribute[1];
+                    if (key) {
+                        key = key[0].toLowerCase() + key.substring(1); // properJavascriptCase TODO: Is this best practice????
+                        result[key.trim()] = value.trim();
                     }
-
-                    if(field.match(regFlags)){
-                        currField['fieldFlags'] = field.match(regFlags)[1].trim()|| '';
-                    }else{ 
-                        currField['fieldFlags'] = '';
-                    }
-
-                    currField['fieldValue'] = '';
-                    
-                    fieldArray.push(currField);
                 });
-                
-                return callback(null, fieldArray);
+                return result;
             });
-        },
-        
-        generateFDFTemplate: function( sourceFile, nameRegex, callback ){
-            this.generateFieldJson(sourceFile, nameRegex, function(err, _form_fields){
-                if (err) {
-                  console.log('exec error: ' + err);
-                  return callback(err, null);
+            return callback(null, formFields);
+        });
+    },
+
+    fillForm: function (sourceFile, destinationFile, data, callback) {
+
+        //Generate the data from the field values.
+        var fdfData = fdf.generate(data),
+            tempFdfFile = "data." + (new Date().getTime()) + ".fdf";
+        //Write the temp fdf file.
+        fs.writeFile(tempFdfFile, fdfData, function (err) {
+            if (err) {
+                return callback(err);
+            }
+
+            child_process.exec("pdftk " + sourceFile + " fill_form " + tempFdfFile + " output " + destinationFile + " flatten", function (error, stdout, stderr) {
+               if (error) {
+                    console.log('exec error: ' + error);
+                    return callback(error);
                 }
-                var _keys   = _.pluck(_form_fields, 'title'),
-            	    _values = _.pluck(_form_fields, 'fieldValue'),
-                    jsonObj = _.zipObject(_keys, _values);
-                
-                return callback(null, jsonObj);
-                
-            });
-        },
-
-        fillForm: function( sourceFile, destinationFile, fieldValues, callback ) {
-
-            //Generate the data from the field values.
-            var formData = fdf.generate( fieldValues ),
-                tempFDF = "data" + (new Date().getTime()) + ".fdf";
-
-            //Write the temp fdf file.
-            fs.writeFile( tempFDF, formData, function( err ) {
-
-                if ( err ) {
-                    return callback(err);
-                }
-
-                child_process.exec( "pdftk " + sourceFile + " fill_form " + tempFDF + " output " + destinationFile + " flatten", function (error, stdout, stderr) {
-
-                    if ( error ) {
-                      console.log('exec error: ' + error);
-                      return callback(error);
+                //Delete the temporary fdf file.
+                fs.unlink(tempFdfFile, function (err) {
+                    if (err) {
+                        return callback(err);
                     }
-                    //Delete the temporary fdf file.
-                    fs.unlink( tempFDF, function( err ) {
-
-                        if ( err ) {
-                            return callback(err);
-                        }
-                        // console.log( 'Sucessfully deleted temp file ' + tempFDF );
-                        return callback();
-                    });
-                } );
+                    return callback();
+                });
             });
-        }
-    };
+        });
+    }
+};
 
-    module.exports = pdffiller;
+module.exports = pdffiller;
 
-}())
+
--- test/test.js
+++ test/test.js
@@ -7,13 +7,8 @@
 
 var pdfFiller = require('../index'), 
     should = require('should'),
-    expected = require('./expected_data');
-
-var dest2PDF =  "test/test_complete2.pdf",
-    source2PDF = "test/test.pdf",
-    dest1PDF =  "test/test_complete1.pdf",
-    source1PDF = "test/test1.pdf";
-
+    data = require('./data'),
+    mocha = require("mocha");
 
 /**
  * Unit tests
@@ -35,256 +30,54 @@ describe('pdfFiller Tests', function(){
 
         it('should not throw an error when creating test_complete.pdf from test.pdf with filled data', function(done) {
             this.timeout(15000);
-            pdfFiller.fillForm( source2PDF, dest2PDF, _data, function(err) { 
+            pdfFiller.fillForm( data.test1.sourceFile, data.test1.sourceFile + "_completed.pdf", _data, function(err) { 
                 should.not.exist(err);
                 done();
             });
         });
     });
 
-    describe('generateFieldJson()', function(){
-        var _expected = [
-            {
-                "fieldFlags": "0",
-                "title" : "first_name",
-                "fieldValue": "",
-                "fieldType": "Text"
-            },
-            {
-                "fieldFlags": "0",
-                "title" : "last_name",
-                "fieldValue": "",
-                "fieldType": "Text"
-            },
-            {
-                "fieldFlags": "0",
-                "title" : "date",
-                "fieldValue": "",
-                "fieldType": "Text"
-            },
-            {
-                "fieldFlags": "0",
-                "title" : "football",
-                "fieldValue": "",
-                "fieldType": "Button"
-            },
-            {
-                "fieldFlags": "0",
-                "title" : "baseball",
-                "fieldValue": "",
-                "fieldType": "Button"
-            },
-            {
-                "fieldFlags": "0",
-                "title" : "basketball",
-                "fieldValue": "",
-                "fieldType": "Button"
-            },
-            {
-                "fieldFlags": "0",
-                "title" : "nascar",
-                "fieldValue": "",
-                "fieldType": "Button"
-            },
-            {
-                "fieldFlags": "0",
-                "title" : "hockey",
-                "fieldValue": "",
-                "fieldType": "Button"
-            }
-        ];
+    describe('getFormFields', function(){
 
-        it('should generate form field JSON as expected', function(done){
+        it('should generate form fields as expected test1', function(done){
             this.timeout(15000);
-            pdfFiller.generateFieldJson( source2PDF, null, function(err, form_fields) { 
+            pdfFiller.getFormFields( data.test1.sourceFile,  function(err, form_fields) { 
                 should.not.exist(err);
-                form_fields.should.eql(_expected);
+                form_fields.should.eql(data.test1.expected.formFields);
                 done();
             });
         });
 
-        it('should generate another form field JSON with no errors', function(done){
+        it('should generate form fields as expected test2', function(done){
             this.timeout(15000);
-            pdfFiller.generateFieldJson( source1PDF, null, function(err, form_fields) { 
+            pdfFiller.getFormFields( data.test2.sourceFile, function(err, form_fields) { 
                 should.not.exist(err);
-                form_fields.should.eql(expected.test1.form_fields);
+                form_fields.should.eql(data.test2.expected.formFields);
                 done();
             });
         });
     });
 
-    describe('generateFDFTemplate()', function(){
-
-        var _expected = {
-            "last_name" : "",
-            "first_name" : "",
-            "date" : "",
-            "football" : "",
-            "baseball" : "",
-            "basketball" : "",
-            "hockey" : "",
-            "nascar" : ""
-        };
-
-        it('should generate a FDF Template as expected', function(done){
+    describe('getFDF()', function(){
+        it('should generate FDF for source pdf 1', function(done){
             this.timeout(15000);
-            pdfFiller.generateFDFTemplate( source2PDF, null, function(err, fdfTemplate) { 
+            pdfFiller.getFdf( data.test1.sourceFile, function(err, fdfTemplate) { 
                 should.not.exist(err);
-                fdfTemplate.should.eql(_expected);
+                fdfTemplate.should.eql(data.test1.expected.fdf);
                 done();
             });
         });
 
-        it('should generate another FDF Template with no errors', function(done){
+        it('should generate FDF for source pdf 2', function(done){
             this.timeout(15000);
-            pdfFiller.generateFDFTemplate( source1PDF, null, function(err, fdfTemplate) { 
+            pdfFiller.getFdf( data.test2.sourceFile, function(err, fdfTemplate) { 
                 should.not.exist(err);
-                fdfTemplate.should.not.eql(expected.test1.fdfTemplate);
+                fdfTemplate.should.eql(data.test2.expected.fdf);
                 done();
             });
         });
+
     });
-
-    describe('convFieldJson2FDF()', function(){
-
-        var _expected = {
-            "first_name" : "John",
-            "last_name" : "Doe",
-            "date" : "Jan 1, 2013",
-            "football" : "Off",
-            "baseball" : "Yes",
-            "basketball" : "Off",
-            "hockey" : "Yes",
-            "nascar" : "Off"
-        };
-        
-        var _data = [
-            {
-                "title" : "first_name",
-                "fieldfieldType": "Text",
-                "fieldValue": "John"
-            },
-            {
-                "title" : "last_name",
-                "fieldfieldType": "Text",
-                "fieldValue": "Doe"
-            },
-            {
-                "title" : "date",
-                "fieldType": "Text",
-                "fieldValue": "Jan 1, 2013"
-            },
-            {
-                "title" : "football",
-                "fieldType": "Button",
-                "fieldValue": false
-            },
-            {
-                "title" : "baseball",
-                "fieldType": "Button",
-                "fieldValue": true
-            },
-            {
-                "title" : "basketball",
-                "fieldType": "Button",
-                "fieldValue": false
-            },
-            {
-                "title" : "hockey",
-                "fieldType": "Button",
-                "fieldValue": true
-            },
-            {
-                "title" : "nascar",
-                "fieldType": "Button",
-                "fieldValue": false
-            }
-        ];
-        
-        it('Should generate an corresponding FDF object', function(done){
-            var FDFData = pdfFiller.convFieldJson2FDF( _data );
-            should.exist(FDFData);
-            should.exist(_expected);
-            (FDFData).should.eql(_expected);
-            done();
-        });
-    });
-
-    describe('mapForm2PDF()', function(){
-
-        var _convMap = {
-            "lastName": "last_name",
-            "firstName": "first_name",
-            "Date": "date",
-            "footballField": "football",
-            "baseballField": "baseball",
-            "bballField": "basketball",
-            "hockeyField": "hockey",
-            "nascarField": "nascar"
-        };
-        
-        var _data = [
-            {
-                "title" : "lastName",
-                "fieldfieldType": "Text",
-                "fieldValue": "John"
-            },
-            {
-                "title" : "firstName",
-                "fieldfieldType": "Text",
-                "fieldValue": "Doe"
-            },
-            {
-                "title" : "Date",
-                "fieldType": "Text",
-                "fieldValue": "Jan 1, 2013"
-            },
-            {
-                "title" : "footballField",
-                "fieldType": "Button",
-                "fieldValue": false
-            },
-            {
-                "title" : "baseballField",
-                "fieldType": "Button",
-                "fieldValue": true
-            },
-            {
-                "title" : "bballField",
-                "fieldType": "Button",
-                "fieldValue": false
-            },
-            {
-                "title" : "hockeyField",
-                "fieldType": "Button",
-                "fieldValue": true
-            },
-            {
-                "title" : "nascarField",
-                "fieldType": "Button",
-                "fieldValue": false
-            }
-        ];
-        
-        var _expected = {
-            "last_name" : "John",
-            "first_name" : "Doe",
-            "date" : "Jan 1, 2013",
-            "football" : "Off",
-            "baseball" : "Yes",
-            "basketball" : "Off",
-            "hockey" : "Yes",
-            "nascar" : "Off"
-        };
-
-        it('Should convert formJson to FDF data as expected', function(done){
-            var convertedFDF = pdfFiller.mapForm2PDF( _data, _convMap );
-            should.exist(convertedFDF);
-            (convertedFDF).should.eql(_expected);
-            done();
-        });
-    });
-
 });
 
 
