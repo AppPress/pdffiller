59b39576ade1e6ed73a5c0032e8ac9d70363d809
Joe Doyle <joe@app-press.com>
2016-01-21 16:55:32 -0800

    Converted to ES6 syntax, minor clean up

--- index.js
+++ index.js
@@ -7,79 +7,80 @@
 *                PDF file with the form fields populated.
 */
 "use strict";
-var child_process = require("child_process"),
-    exec = child_process.exec,
-    spawn = child_process.spawn,
-    fdf = require("fdf"),
-    util = require("util"),
-    fs = require("fs");
+
+const child_process = require("child_process");
+const exec = child_process.exec;
+const spawn = child_process.spawn;
+const fdf = require("fdf");
 
 var pdffiller = {
 
-    getFdf: function (sourceFile, callback) {
-        var pdftkCommand = "pdftk " + sourceFile + " generate_fdf output -";
-        exec(pdftkCommand, function (err, stdout, stderr) {
-            if (err) {
-                console.log(err);
-                return callback(err);
-            }
-            return callback(null, stdout.toString());
+	getFdf: function (sourceFile, callback) {
+		const pdftkCommand = `pdftk ${sourceFile} generate_fdf output -`;
 
+		exec(pdftkCommand, (err, stdout, stderr) => {
+			if (err) {
+				console.log(err);
+				return callback(err);
+			}
 
-        });
-    },
+			return callback(null, stdout.toString());
+		});
+	},
 
-    getFormFields: function (sourceFile, callback) {
-        var pdftkCommand = "pdftk " + sourceFile + " dump_data_fields_utf8";
-        exec(pdftkCommand, function (err, stdout, stderr) {
-            if (err) {
-                console.log(err);
-                return callback(err);
-            }
-            var fieldSections = stdout.toString().split("---").slice(1);
-            var formFields = fieldSections.map(function (fieldSection) {
-                var fieldAttributes = fieldSection.split(/\n/);
-                var result = {};
-                fieldAttributes.forEach(function (fieldAttribute) {
-                    var packedAttribute = fieldAttribute.split(/:/);
-                    var key = packedAttribute[0];
-                    var value = packedAttribute[1];
-                    if (key) {
-                        key = key[0].toLowerCase() + key.substring(1); // properJavascriptCase TODO: Is this best practice????
-                        result[key.trim()] = value.trim();
-                    }
-                });
-                return result;
-            });
-            return callback(null, formFields);
-        });
-    },
+	getFormFields: function (sourceFile, callback) {
+		const pdftkCommand = `pdftk ${sourceFile} dump_data_fields_utf8`;
 
-    fillForm: function (inputFile, data, callback) {
-        // Generate FDF template from data
-        var fdfData = fdf.generate(data);
-        var child = child_process.spawn("pdftk", [inputFile, "fill_form", "-","output", "-", "flatten"]);
-        var buffers = [];
-        var buffersLength = 0;
-        child.stdout.on("data", function(data){
-            buffers.push(data);
-            buffersLength += data.length;
-         });
-        
-        child.on("error", function(err){
-            return callback(err);
-        });
-        
-        child.on("close", function(){
-            data = Buffer.concat(buffers, buffersLength);
-            return callback(null, data);
-        });
-        child.stdin.write(fdfData);
-        child.stdin.end();
-    }
+		exec(pdftkCommand, (err, stdout, stderr) => {
+			if (err) {
+				console.log(err);
+				return callback(err);
+			}
+
+			const fieldSections = stdout.toString().split("---").slice(1);
+			const formFields = fieldSections.map((fieldSection) => {
+				const fieldAttributes = fieldSection.split(/\n/);
+				const result = {};
+				fieldAttributes.forEach((fieldAttribute) => {
+					const packedAttribute = fieldAttribute.split(/:/);
+					let key = packedAttribute[0];
+					let value = packedAttribute[1];
+
+					if (key) {
+						key = key[0].toLowerCase() + key.substring(1);
+						result[key.trim()] = value.trim();
+					}
+				});
+
+				return result;
+			});
+
+			return callback(null, formFields);
+		});
+	},
+
+	fillForm: function (inputFile, data, callback) {
+		// Generate FDF template from data
+		const fdfData = fdf.generate(data);
+		const child = spawn("pdftk", [inputFile, "fill_form", "-","output", "-", "flatten"]);
+		let buffers = [];
+		let buffersLength = 0;
+		child.stdout.on("data", (data) => {
+			buffers.push(data);
+			buffersLength += data.length;
+		});
+
+		child.on("error", (err) => {
+			return callback(err);
+		});
+
+		child.on("close", () => {
+			data = Buffer.concat(buffers, buffersLength);
+			return callback(null, data);
+		});
+		child.stdin.write(fdfData);
+		child.stdin.end();
+	}
 };
 
 module.exports = pdffiller;
-
-
-
